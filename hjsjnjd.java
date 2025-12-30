eeaf69bffe2b04de2cab95bb8ec13595cd13a272
siva rushinadh <karanamrushi834@gmail.com>
2025-12-30 21:19:54 +0530

    Add files via upload

---
+++ hjsjnjd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class hjsjnjd {
+    public static void main(String... args){
+        Scanner s=new Scanner(System.in);
+     int m=s.nextInt();   
+        int[] arr=new int[m];
+        for(int i=0;i<=5;i++){
+        arr[i]=s.nextInt();
+        System.out.println(Arrays.toString(arr));
+    }
+}
+}
